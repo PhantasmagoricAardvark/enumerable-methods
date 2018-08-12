af6b04094e80e92a7f7d8a550b21a0a49e074d08
seba <villeaho@tutanota.com>
2018-08-12 20:38:54 +0300

    Added map and inject and also multiply_els.

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -73,7 +73,6 @@ module Enumerable
 				if yield(self[i]) == true
 					counter += 1
 				end
-
 				i += 1
 			end
 			return p counter
@@ -82,10 +81,43 @@ module Enumerable
 		end
 	end
 
+	def my_map
+		i = 0
+		arr = []
+		while i < self.length
+			arr.push(yield(self[i]))
+			i += 1
+
+		end
+		arr
+	end
+
+	def my_inject
+		i = 0
+		arr = []
+		counter = self[0]
+		if block_given?
+			while i < self.length
+				if self[i + 1] != nil
+					arr.push(yield(counter, self[i + 1]))
+					counter = yield(counter, self[i + 1])
+				end
+				i += 1
+			end
+			arr[-1]				
+
+		end
+	end
+
 	
 
-
 end	
+
+def multiply_els(arr)
+	arr.my_inject do |accum, i|
+		accum * i
+	end
+end
 
 plus_one = Proc.new { |e, i| puts "e is #{e}"
 puts "index is #{i}" 
@@ -93,10 +125,10 @@ puts }
 
 evens = Proc.new { |i| i.even? }
 greater_than = Proc.new { |i| i >= 1}
+squared = Proc.new { |i| i * i}
 
 
-
-[0,2,-10,-3].my_count?(&greater_than)
+p [1,2,-10,-3].my_map{ |i| i * i}
 	
 	
 
